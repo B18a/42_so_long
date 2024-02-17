b1c09ff2b9e53ac168b7abed216f26dfeeb9f491
Andreas Jehle <ajehle@2-D-4.42heilbronn.de>
2024-02-17 11:15:28 +0100

    t_player t_game

--- include/header.h
+++ include/header.h
@@ -6,7 +6,7 @@
 /*   By: ajehle <ajehle@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 12:14:01 by ajehle            #+#    #+#             */
-/*   Updated: 2024/02/16 16:33:12 by ajehle           ###   ########.fr       */
+/*   Updated: 2024/02/17 10:09:23 by ajehle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,25 @@
 #include <unistd.h>
 
 #include"../MLX42/include/MLX42/MLX42.h"
+
+
+typedef struct	s_player
+{
+	int				x_pos_player;
+	int				y_pos_player;
+	mlx_texture_t	*texture;
+	mlx_image_t		*image;
+}				t_player;
+
+typedef struct	s_game
+{
+	mlx_t		*game_window;
+	mlx_image_t	*game_image;
+	t_player	*player;
+}				t_game;
+
+
+
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,14 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andreasjehle <andreasjehle@student.42.f    +#+  +:+       +#+        */
+/*   By: ajehle <ajehle@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 12:09:33 by ajehle            #+#    #+#             */
-/*   Updated: 2024/02/16 20:28:13 by andreasjehl      ###   ########.fr       */
+/*   Updated: 2024/02/17 10:27:15 by ajehle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/header.h"
+
+
+
 
 #define WIDTH	500
 #define HEIGHT	500
@@ -21,46 +24,107 @@ static void ft_hook(void* param)
 		mlx_close_window(param);
 }
 
+// void	ft_key_hook(mlx_key_data_t *key, void *data)
+// {
+
+// }
+
+t_player *ft_initialize_player()
+{
+	t_player	*player;
+
+	player = malloc(sizeof(t_player));
+	player->x_pos_player = 300;
+	player->y_pos_player = 300;
+	
+
+	return (player);
+}
+
+int	ft_exit_player(t_player *player, mlx_t *window, mlx_image_t *image)
+{
+	if(player)
+		free(player);
+	if(window)
+		free(window);
+	if(image)
+		free(image);
+	return(1);
+}
+
+int	ft_exit_game(t_game *game)
+{
+	if(game->game_image)
+		free(game->game_image);
+	if(game->game_window)
+		free(game->game_window);
+	if(game)
+		free(game);
+	return(1);
+}
+
+
+t_game	*ft_initialize_game()
+{
+	t_game	*game;
+
+	game = malloc(sizeof(t_game));
+	if(!game)
+		return(ft_exit_game(game), NULL);
+	game->game_window = malloc(sizeof(mlx_t));
+	if(!game->game_window)
+		return(ft_exit_game(game), NULL);
+	game->game_image = malloc(sizeof(mlx_image_t));
+	if(!game->game_image)
+		return(ft_exit_game(game), NULL);
+	return (game);
+}
 
 int	main(void)
 {
 	mlx_t	*window;
 	mlx_image_t	*image;
-	int x;
 
-	x = 0;
+	t_player	*player;
+	player = ft_initialize_player();
+	if(!player)
+		return(ft_exit_player(player, NULL, NULL));
+
+	t_game	*game;
+	game = ft_initialize_game();
+	if (!game)
+		return(ft_exit_player(player, NULL, NULL));
+
 
 	window = mlx_init(WIDTH, HEIGHT, "test", false);
 	if (!window)
-		return(1);
+		return(ft_exit_player(player, window, NULL));
 
 	image = mlx_new_image(window, WIDTH, HEIGHT);
 	if (!image)
-		return (1);
+		return (ft_exit_player(player, window, image));
 	if (mlx_image_to_window(window, image, 100, 0) == -1)
-		return (1);
+		return (ft_exit_player(player, window, image));
 
+
+
+	int x;
+	x = 0;
 	while (x < 300)
 	{
 		x++;
 		mlx_put_pixel(image, x, 100, 0xFF00F0FF);
 	}
 
-	//This part here is to display a picture on the scree
-	int x_pos_player = 100;
-	int y_pos_player = 100;
-	mlx_texture_t* link;
-	link = mlx_load_png("./assets/Link.png");
+	player->texture = mlx_load_png("./assets/Link.png");
+	player->image = mlx_texture_to_image(window, player->texture);
 
-	mlx_image_t* link_img;
-	link_img = mlx_texture_to_image(window, link);
-
-	mlx_image_to_window(window, link_img, x_pos_player, y_pos_player);
+	mlx_image_to_window(window, player->image, player->x_pos_player, player->y_pos_player);
 	//*******************************************************
-	
+
+	// mlx_key_hook(window, ft_key_hook, player);
 	mlx_loop_hook(window, ft_hook, window);
 
-	
 	mlx_loop(window);
 	// mlx_terminate(window);
 
