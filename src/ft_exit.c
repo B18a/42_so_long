1607232e207b4e4cbb00344315c1c40cea0e34c8
Andreas Jehle <ajehle@3-D-1.42heilbronn.de>
2024-03-08 16:26:27 +0100

    norminette exit.c

--- src/ft_exit.c
+++ src/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: ajehle <ajehle@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 09:47:22 by ajehle            #+#    #+#             */
-/*   Updated: 2024/03/08 16:17:44 by ajehle           ###   ########.fr       */
+/*   Updated: 2024/03/08 16:26:05 by ajehle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,31 @@ int	ft_exit_pos(t_pos *pos)
 	return (1);
 }
 
-
-
-void ft_exit_player(t_game *game)
+void	ft_exit_player(t_game *game)
 {
 	if (game->player->pos)
 		ft_exit_pos(game->player->pos);
 	if (game->player->texture)
 		mlx_delete_texture(game->player->texture);
 	free(game->player);
-
 }
-void ft_exit_enemy(t_game *game)
+
+void	ft_exit_enemy(t_game *game)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(i < AMOUNT_ENEMY)
+	while (i < AMOUNT_ENEMY)
 	{
-
-			if (game->enemy[i]->pos)
-				ft_exit_pos(game->enemy[i]->pos);
-			if (game->enemy[i]->texture)
-				mlx_delete_texture(game->enemy[i]->texture);
-
-			free(game->enemy[i]);
+		if (game->enemy[i]->pos)
+			ft_exit_pos(game->enemy[i]->pos);
+		if (game->enemy[i]->texture)
+			mlx_delete_texture(game->enemy[i]->texture);
+		free(game->enemy[i]);
 		i++;
 	}
 	free(game->enemy);
 }
-
 
 int	ft_exit_game(t_game *game)
 {
