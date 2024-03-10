fb2984411f74614eb3ba5e26d68d4a64e2dc7189
B18a <andreasjehle89@web.de>
2024-03-10 11:09:45 +0100

    test

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: andreasjehle <andreasjehle@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 14:20:06 by ajehle            #+#    #+#             */
-/*   Updated: 2024/03/09 17:13:27 by andreasjehl      ###   ########.fr       */
+/*   Updated: 2024/03/10 11:09:27 by andreasjehl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ char	**parse_input(char *path_map)
 {
 	int		fd;
 	char	*map_in_line;
+	char	*map_in_line_temp;
 	char	**map_in_arr;
 	
 	map_in_line = ft_strdup("");
+	map_in_line_temp = ft_strdup("");
 	map_in_arr = NULL;
 
 	printf("%s\n",path_map);
@@ -28,28 +30,18 @@ char	**parse_input(char *path_map)
 		printf("Opening File failed\n");
 		return (NULL);
 	}
-	
-	// while (map_in_line)
-	while (map_in_line)
+	while ((map_in_line_temp = get_next_line(fd)))
 	{
-		map_in_line = get_next_line(fd);
-		
-		if(map_in_line)
-			printf("%s\n",map_in_line);
-		free(map_in_line);
-		if(!map_in_line)
-		{
-			free(map_in_line);
-			break;	
-		}
-	
+			// map_in_line_temp = get_next_line(fd);
+			// printf("%s\n",map_in_line_temp);
+			map_in_line = ft_strjoin(map_in_line, map_in_line_temp);
+			// printf("%s\n",map_in_line);
+			free(map_in_line_temp);
+			map_in_line_temp = NULL;
 	}
-	
-
-	
-
+	// printf("%s\n",map_in_line);
+	free(map_in_line_temp);
+	// free(map_in_line);
 	close(fd);
-	if(map_in_line != NULL)
-	free(map_in_line);
 	return(map_in_arr);
 }
