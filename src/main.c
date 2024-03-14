0a4ed48412d09810ab714b7a157e148b303b268e
B18a <andreasjehle89@web.de>
2024-03-14 14:45:12 +0100

    end

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: andreasjehle <andreasjehle@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 12:09:33 by ajehle            #+#    #+#             */
-/*   Updated: 2024/03/14 14:16:42 by andreasjehl      ###   ########.fr       */
+/*   Updated: 2024/03/14 14:44:59 by andreasjehl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,24 @@ int	check_file_type(char *str)
 	return(0);
 }
 
+void	call_exit_prep(char *map_as_string, char **map_as_arr)
+{
+	int i;
+	
+	i = 0;
+	if(map_as_string)
+		free(map_as_string);
+	if(map_as_arr)
+	{
+	while(map_as_arr[i])
+	{
+		free(map_as_arr[i]);
+		i++;
+	}
+	free(map_as_arr);
+	}
+}
+
 int	main(int argc, char**argv)
 {
 	atexit(check_leaks);
@@ -119,6 +137,7 @@ int	main(int argc, char**argv)
 	if(map_char_check(map_as_string) > 0)
 	{
 		printf("map_char_check \n");
+		call_exit_prep(map_as_string, map_as_arr);
 		return(0);
 	}	
 	map_as_arr = ft_split(map_as_string, '\n');		
@@ -127,38 +146,39 @@ int	main(int argc, char**argv)
 	if(map_arr_check(map_as_arr) > 0)
 	{
 		printf("map_arr_check \n");
+		call_exit_prep(map_as_string, map_as_arr);
 		return(0);
 	}
 
-	if(map_as_string)
-		free(map_as_string);
-	if(map_as_arr)
-		free_map_in_arr(map_as_arr);
 	// game = NULL;
 	// game = ft_initialize_game();
 	// if(!game)
+	// {
+	// 	call_exit_prep(map_as_string, map_as_arr);
 	// 	return(0);
+	// }
 
 
 
 	// ft_init_pos_asset(game, game->enemy , game->enemy_total);
 	// ft_init_pos_asset(game, game->item , game->item_total);
-	// /**************************************/
+	/**************************************/
 
 	// game->game_window = mlx_init(WIDTH, HEIGHT, NAME_WINDOW, true);
 	// if(!game->game_window)
 	// 	return (call_exit(game), 0);
 
-	// // ft_show_address(game);
+	// ft_show_address(game);
 
 
 	// ft_load_textures_player(game, 0, 0);
-	// // ft_load_textures_asset(game, game->enemy, paths_enem, game->enemy_total, (t_pos){100, 100});
-	// // ft_load_textures_asset(game, game->item, paths_item, game->item_total,(t_pos){300, 10});
-	// // ft_load_textures_exit(game, PATH_EXIT, (t_pos){0, 400});
+	// ft_load_textures_asset(game, game->enemy, paths_enem, game->enemy_total, (t_pos){100, 100});
+	// ft_load_textures_asset(game, game->item, paths_item, game->item_total,(t_pos){300, 10});
+	// ft_load_textures_exit(game, PATH_EXIT, (t_pos){0, 400});
 
 
-	// // start_game(game);
+	// start_game(game);
+	call_exit_prep(map_as_string, map_as_arr);
 	// call_exit(game);
 
 	return (0);
